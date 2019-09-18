1a60d63393ac55d84edfcb1f1998c1648f7b6765
timbuk <ivoschwizer@gmail.com>
2019-09-18 17:57:13 +0200

    timer new

--- scripts/main.js
+++ scripts/main.js
@@ -2,34 +2,21 @@ $(document).ready(function () {
     
     
     // timer countdown
+    var countDownDate = new Date("21 September 2019 17:50:00 GMT+02:00").getTime();
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("countdowntimer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        if (distance < 0) {
+            $('#countdown').fadeOut("slow");
+        }
+    }, 1000);
     
-    function makeTimer() {
-
-	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-		var endTime = new Date("18 September 2019 16:22:00 GMT+01:00");			
-			endTime = (Date.parse(endTime) / 1000);
-
-			var now = new Date();
-			now = (Date.parse(now) / 1000);
-
-			var timeLeft = endTime - now;
-
-			var days = Math.floor(timeLeft / 86400); 
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-  
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
-
-			$("#days").html(days + "<span>Days</span>");
-			$("#hours").html(hours + "<span>Hours</span>");
-			$("#minutes").html(minutes + "<span>Minutes</span>");
-			$("#seconds").html(seconds + "<span>Seconds</span>");		
-	}
-
-	setInterval(function() { makeTimer(); }, 1000);
     
     //Responsive Navigation
     
