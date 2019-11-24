f56381d552a95bf0702d5160ab066ea120ab3379
timbuk <ivoschwizer@gmail.com>
2019-11-25 00:08:56 +0100

    timer delite

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,5 @@
 $(document).ready(function () {
     
-    
-    // timer countdown
-    
-    function getNormalisedDatetime(dString) { // yyyy-mm-dd hh:mm:ss
-        var parts = dString.split(" ");
-        var dParts = parts[0].split("-");
-        var tParts = parts[1].split(":");
-        return new Date(dParts[0],dParts[1]-1,dParts[2],tParts[0],tParts[1],tParts[2]);
-    }
-    function pad(num) {
-        return ("0"+num).slice(-2);
-    }
-    
-    // Set the date we're counting down to
-    var countDownDate = getNormalisedDatetime("2019-11-25 00:00:00").getTime();
-    
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-        
-        // Get todays date and time
-        var now = new Date().getTime();
-        
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-        
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        
-        // Display the result in the element with id="countdown"
-        document.getElementById("countdowntimer").innerHTML = days + " Tagen " + hours + " Stunden " + minutes + " Minuten " + seconds + " Sekunden ";
-        
-        // If the count down is finished, write some text 
-        if (distance < 0) {
-            $('#countdown').fadeOut("slow");
-        }
-    }, 1000);
-
-    
-    
     //Responsive Navigation
     
     $('.menu').click(function() {
